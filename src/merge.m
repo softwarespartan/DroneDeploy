4565288b221a582f54a0864b2031b28185364ce4
Abel Brown <brown.2179@gmail.com>
2016-02-13 13:31:23 -0500

    updated merge deals with world limits

--- src/merge.m
+++ src/merge.m
@@ -7,7 +7,7 @@ function I = merge(I1, K1, I2, K2)
     R1 = projective2d(inv(Rxy(O1)));  R2 = projective2d(inv(Rxy(O2)));
 
     % rectify each image
-    I1 = imwarp(I1,R1); I2 = imwarp(I2,R2);
+    [I1,view1] = imwarp(I1,R1); [I2,view2] = imwarp(I2,R2);
 
     % create grey scale images for feature extraction
     I1gray=rgb2gray(I1);  I2gray=rgb2gray(I2);
@@ -21,22 +21,26 @@ function I = merge(I1, K1, I2, K2)
     [features2, validBlobs2] = extractFeatures(I2gray, blobs2);
     
     % match the features of each image to one another
-    indexPairs = matchFeatures(features1, features2, 'Metric', 'SAD', 'MatchThreshold', 5);
+    indexPairs = matchFeatures(features1, features2, 'Metric', 'SAD', 'MatchThreshold', 5,'Unique',1);
     
     % turn the index into valid blobs
     matchedPoints1 = validBlobs1(indexPairs(:,1),:);
     matchedPoints2 = validBlobs2(indexPairs(:,2),:);
     
     [fMatrix, epipolarInliers, status]...
-    = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, ...
+        = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, ...
                                 'Method'           , 'RANSAC' , ...
                                 'NumTrials'        , 2000     , ...
-                                'DistanceThreshold', 0.2      , ...
-                                'Confidence'       , 99.99      ...
+                                'DistanceThreshold', 0.1      , ...
+                                'Confidence'       , 99.        ...
                                );
                            
-    if status ~= 0 || isEpipoleInImage(fMatrix, size(I1)) || isEpipoleInImage(fMatrix', size(I2))
-        error('Either not enough matching points were found ');
+    if status ~= 0 
+        error('fundemental matrix calculation failed');
+    end
+    
+    if (~isEpipoleInImage(fMatrix, size(I1)) || ~isEpipoleInImage(fMatrix', size(I2)))
+        warning('epipole not in Image')
     end
 
     inlierPoints1 = matchedPoints1(epipolarInliers, :);
@@ -45,18 +49,17 @@ function I = merge(I1, K1, I2, K2)
     % compute the transformation between matched features
     tform = estimateGeometricTransform(inlierPoints1, inlierPoints2, 'projective');
     
-    outputview = imref2d(size(I1));
-    I1r = imwarp(I1,tform,'outputView',outputview);
+    %outputview = imref2d(size(I1));
+    [I1r,view1r] = imwarp(I1,tform);
 
+    view2r = imref2d(size(I2));
+    
     % calculate the images size (either image work since they are same size)
-    imageSize = size(I1);
+    xMin = min([view1r.XWorldLimits, view2r.XWorldLimits]);
+    xMax = max([view1r.XWorldLimits, view2r.XWorldLimits]);
     
-    % compute the combined image limits
-    [xlim, ylim] = outputLimits(tform, [1 imageSize(2)], [1 imageSize(1)]);
-    
-    % compute xy-dims
-    xMin = min([1; xlim(:)]);  xMax = max([imageSize(2); xlim(:)]);
-    yMin = min([1; ylim(:)]);  yMax = max([imageSize(1); ylim(:)]);
+    yMin = min([view1r.YWorldLimits, view2r.YWorldLimits]);
+    yMax = max([view1r.YWorldLimits, view2r.YWorldLimits]);
     
     % Width and height of panorama.
     width  = round(xMax - xMin);  height = round(yMax - yMin);
@@ -64,28 +67,37 @@ function I = merge(I1, K1, I2, K2)
     % create x and y limits`
     xLimits = [xMin xMax];  yLimits = [yMin yMax];
     
-    % Initialize the "empty" panorama.
+    % Initialize the "empty" mosaic.
     I = zeros([height width 3], 'like', I1);
     
     % initialize 
     blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');
     
     % init empty view
-    panoramaView = imref2d([height width], xLimits, yLimits);
+    mosaicView = imref2d([height width], xLimits, yLimits);
     
-    % add image 1
-    warpedImage = imwarp(I1r, projective2d(eye(3)), 'OutputView', panoramaView);
-    I = step(blender, I, warpedImage, warpedImage(:,:,1));
+    % add image 1 
+    wI = imwarp(I1, tform, 'OutputView', mosaicView);
+    I = step(blender, I, wI, wI(:,:,1));
     
     % add image 2
-    warpedImage = imwarp(I2, projective2d(eye(3)), 'OutputView', panoramaView);
-    I = step(blender, I, warpedImage, warpedImage(:,:,1));
+    wI = imwarp(I2, projective2d(eye(3)), 'OutputView', mosaicView);
+    I = step(blender, I, wI, wI(:,:,1));
     
-    % create plots
+    % compute empty space
+    ix = (I(:,:,1)==0) & (I(:,:,2)==0)& (I(:,:,3)==0);  
+    
+    % figure out empty rows and columns
+    ix1 = ~all(ix); ix2 = ~all(ix,2);
+    
+    % trim the image
+    I = I(ix2,ix1,:);
+    
+    % create plots (or not)
     if(1)
        
         % show the rectified images
-        imshowpair(I1,I2,'montage');
+        figure;imshowpair(I1,I2,'montage');
         
         % show matches points
         figure;
@@ -97,11 +109,9 @@ function I = merge(I1, K1, I2, K2)
         legend('epipolar matches in I1', 'epipolar matches in I2');
         
         % show images transformed overlay with false coloring
-        figure, imshowpair(I1r,I2)
+        figure, imshowpair(I1r,view1r,I2,imref2d(size(I2)))
         
         % show result
         figure;  imshow(I)
     end
-    
-    
 end
